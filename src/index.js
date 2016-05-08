c6cc7b99c3bdf02c4c321ff260c94de46ff98719
Paul Sonnentag <Paul.Sonnentag@googlemail.com>
2016-05-08 18:31:12 +0200

    Prevent default scrolling

--- src/index.js
+++ src/index.js
@@ -2,4 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {App} from './components/app';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+var appElement = document.getElementById('app');
+
+ReactDOM.render(<App />, appElement);
+
+// prevent default scrolling when touching app
+appElement.addEventListener('touchmove', e => e.preventDefault());
