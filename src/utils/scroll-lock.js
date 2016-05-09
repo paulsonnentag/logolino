6c93b006641f0517dfbe499db935d564ca1076ea
Paul Sonnentag <Paul.Sonnentag@googlemail.com>
2016-05-09 09:23:54 +0200

    Fix scroll-lock: enable scrolling for every child of scrollable

--- src/utils/scroll-lock.js
+++ src/utils/scroll-lock.js
@@ -1,13 +1,20 @@
-/* scrollLock (element)
- * disables scrolling for fixed sized Elements
+/* disables scrolling for fixed sized Elements
  * enable scrolling for children by setting data-scrollable to true
  * */
 export default function scrollLock (element) {
 
   element.addEventListener('touchmove', (evt) => {
 
-    if (!evt.target.dataset.scrollable) {
+    if (!isScrollable(evt.target)) {
       evt.preventDefault();
     }
   });
+}
+
+// check if element or any of its parents has data-scrollable set
+function isScrollable (element) {
+  if (element === null) {
+    return false;
+  }
+  return element.dataset.scrollable || isScrollable(element.parentNode);
 }
